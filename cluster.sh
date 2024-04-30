8ae74b0e210ae8c0bcf54b35dc90b150972905b3
Nick Waters <watersn@mskcc.org>
2024-04-29 22:11:29 -0400

    fix bad orient

--- cluster.sh
+++ cluster.sh
@@ -5,10 +5,10 @@ indir=$1
 # from docs
 echo Obtaining Gold reference database for orienting detection
 GOLD=https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.gold.bacteria.zip
-if [ ! -e "${SYTYCS_CACHEDIR}/gold.fasta" ]
+if [ ! -f "${SYTYCS_CACHEDIR}/gold.fasta" ]
 then
 
-    if [ ! -e "${SYTYCS_CACHEDIR}/silva.gold.bacteria.zip" ]; then
+    if [ ! -f "${SYTYCS_CACHEDIR}/silva.gold.bacteria.zip" ]; then
         wget $GOLD -O ${SYTYCS_CACHEDIR}/silva.gold.bacteria.zip
     fi
 
@@ -21,9 +21,14 @@ fi
 
 
 cat $indir/pcr/*.fasta | sed "s| |--|g" > $indir/cluster_input.fasta
+#vsearch --makeudb_usearch ${SYTYCS_CACHEDIR}/gold.fasta --output ${SYTYCS_CACHEDIR}/gold.udb
+#vsearch -orient $indir/cluster_input.fasta -db ${SYTYCS_CACHEDIR}/gold.udb -fastaout $indir/cluster_input.oriented.fasta
+
 vsearch -orient $indir/cluster_input.fasta -db ${SYTYCS_CACHEDIR}/gold.fasta -fastaout $indir/cluster_input.oriented.fasta
+
 vsearch --cluster_fast $indir/cluster_input.oriented.fasta --threads 16  --id 1 --strand both  -uc $indir/clusters.uc
-mafft --auto $indir/cluster_input.fasta > $indir/alignment/amplicons.mafft.aln
+
+mafft --auto $indir/cluster_input.oriented.fasta > $indir/alignment/amplicons.mafft.aln
 # TODO: switch for infernal?
 
 #FastTree -gtr -nt $indir/alignment/amplicons.mafft.aln > $indir/alignment/amplicons.mafft.aln.nwk
